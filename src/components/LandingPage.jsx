1fed7560653a45a4796c588cce7b33b90aa1e757
troycaselli <swordandtrowel1689@gmail.com>
2023-04-14 14:07:29 -0700

    created name header

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,9 +4,19 @@ import '../styling/landingPage.css';
 
 function LandingPage() {
     return(
-        <div>
-            <h1>Intro</h1>
-            <h2 id='projects'>Projects</h2>
+        <div className='landing-wrapper'>
+            <div className='intro-container'>
+                <div className="svg-wrapper">
+                    <svg height="150" width="600" xmlns="http://www.w3.org/2000/svg">
+                        <rect class="shape" height="150" width="600" />
+                        <text x='30' y='95' className="text">TROY CASELLI</text>
+                    </svg>
+                </div>
+            </div>
+            <div  id='projects' className='projects-wrapper'>
+                <h2>Projects</h2>
+
+            </div>
         </div>
     );
 }
