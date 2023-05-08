f99e0c87bf2b176c767c7d0252df7e0864ae97e1
troycaselli <swordandtrowel1689@gmail.com>
2023-05-08 14:38:43 -0700

    resized images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Link, useLocation} from 'react-router-dom';
 
-import image from '../images/logo/sword-spade-large.png';
+import image from '../images/logo/sword-spade-large.webp';
 import '../styling/header.css';
 
 function Header() {
