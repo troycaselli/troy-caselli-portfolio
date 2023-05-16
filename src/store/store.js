fa18cc3bb049935dcc2d982a57b6bbecdf436f7c
troycaselli <swordandtrowel1689@gmail.com>
2023-05-16 11:20:07 -0700

    created theme color variables at css root

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit';
+import themeSlice  from './reducers/themeSlice';
+
+export const store = configureStore({
+    reducer: {
+        theme: themeSlice
+    },
+})
