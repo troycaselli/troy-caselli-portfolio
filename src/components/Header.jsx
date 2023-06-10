99a2e9274f796f8338c3001a70a013359f08231c
troycaselli <swordandtrowel1689@gmail.com>
2023-06-09 19:02:40 -0700

    wip toggling function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Link, useLocation} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 
 import image from '../images/logo/sword-spade-large.webp';
 import '../styling/header.css';
@@ -17,7 +19,32 @@ function Header() {
             setActiveLink(pathname);
         }
     }, [pathname, hash, key]);
+
+    // theme and icon switch
+    const [themeMode, setThemeMode] = useState(<FontAwesomeIcon icon={faSun} />)
+    const toggleThemeIcon = () => {
+        if (themeMode.props.icon.iconName === 'sun') {
+            setThemeMode(<FontAwesomeIcon icon={faMoon} />)
+        } else {
+            setThemeMode(<FontAwesomeIcon icon={faSun} />)
+        }
+    }
     
+    useEffect(() => {
+        const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+        function switchTheme(e) {
+            if (e.target.checked) {
+                document.documentElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('theme', 'dark');
+            }
+            else {
+                document.documentElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('theme', 'light');
+            }
+        }
+        toggleSwitch.addEventListener('change', switchTheme, false);
+    }, [themeMode])
+
     return(
         <div className='header-wrapper'>
             <div className='navlink-wrapper'>
@@ -29,6 +56,10 @@ function Header() {
                     <Link to='about' className={'header-link ' + (activeLink === '/about' ? 'current' : '')}>About Me</Link>
                     <Link to='resume' className={'header-link ' + (activeLink === '/resume' ? 'current' : '')}>Résumé</Link>
                 </div>
+                <label className="theme-switch" for="checkbox">
+                    <input type="checkbox" id="checkbox" />
+                    <div className={localStorage.getItem('theme')} onClick={e => toggleThemeIcon(e)}>{themeMode}</div>
+                </label>
             </div>
             <div className='logo-container'>
                 <img src={image} alt="Logo" className='logo-image'/>
