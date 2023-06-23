3c2d5fff13678714156f9422125816a63db15049
troycaselli <swordandtrowel1689@gmail.com>
2023-06-23 16:00:33 -0700

    initializing loading component files

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styling/loading.css';
+
+function Loading() {
+
+  return (
+    <div>
+        <p>RENDERING</p>
+    </div>
+  );
+}
+
+export default Loading;
