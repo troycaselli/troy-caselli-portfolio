55bf78e9b286b3939cc33793b28fa3ac62cbef29
troycaselli <swordandtrowel1689@gmail.com>
2023-06-29 10:01:28 -0700

    initialized reuseable project page

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styling/projectDetails.css';
+
+function ProjectDetails() {
+
+  return (
+    <div>
+        <p>Faith in Five</p>
+    </div>
+  );
+}
+
+export default ProjectDetails;
