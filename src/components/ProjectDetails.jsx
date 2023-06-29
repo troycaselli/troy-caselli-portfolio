2a81b4e9c6c4285f52c00aa43078314d639540fd
troycaselli <swordandtrowel1689@gmail.com>
2023-06-29 10:19:08 -0700

    optimized image files location and filled out projectDetails component

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -4,8 +4,21 @@ import '../styling/projectDetails.css';
 function ProjectDetails() {
 
   return (
-    <div>
-        <p>Faith in Five</p>
+    <div className='project-wrapper'>
+
+      <div className='project-intro'>
+        <h1 className='project-title'>Faith in Five</h1>
+        <p className='project-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas reprehenderit cupiditate, illum sed delectus ipsa eligendi? Ipsa blanditiis praesentium, sapiente voluptates delectus adipisci nemo nulla, obcaecati modi exercitationem quo nobis!</p>
+        <p className='project-contributions'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ab ratione error. Commodi placeat, ex eius harum atque eum porro dolorem cumque eligendi quisquam modi. Possimus doloremque fugiat reiciendis sapiente!</p>
+      </div>
+
+      <div className='project-wireframes'>
+        
+      </div>
+      
+      <div className='project-user-flow'>
+        
+      </div>
     </div>
   );
 }
