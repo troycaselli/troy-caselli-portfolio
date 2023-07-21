5574d6388886d82ada40681bfb14d2cee7bd7ee3
troycaselli <swordandtrowel1689@gmail.com>
2023-07-21 12:10:53 -0700

    created image blur component

--- src/components/IncrementedImage.jsx
+++ src/components/IncrementedImage.jsx
@@ -1,25 +1,22 @@
 import { useState, useEffect } from "react";
 
-const IncrementedImage = ({ placeholderSrc, src, ...props }) => {
+const IncrementedImage = (props) => {
+  const { placeholderSrc, src, alt, className } = props;
   const [imgSrc, setImgSrc] = useState(placeholderSrc || src);
-  const [imgClass, setImgClass] = useState("slideshow__image__blur");
+  const [imgClass, setImgClass] = useState(className + " blur");
 
-  console.log(imgSrc);
+  console.log(imgClass);
   useEffect(() => {
     const img = new Image();
     img.src = src;
-    setImgClass("slideshow__image");
     img.onload = () => {
+      //   setTimeout(() => {
+      setImgClass(className);
       setImgSrc(src);
+      //   }, 2000);
     };
   }, [src]);
 
-  return (
-    <img
-      {...{ src: imgSrc, ...props }}
-      alt={props.alt || ""}
-      className={imgClass}
-    />
-  );
+  return <img src={imgSrc} alt={alt || ""} className={imgClass} />;
 };
 export default IncrementedImage;
