6a86aac83783c855e3bce0c298bb9c6774863c7d
troycaselli <swordandtrowel1689@gmail.com>
2023-07-28 17:36:55 -0700

    moved header links

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -19,48 +19,44 @@ function Header() {
 
   return (
     <div className="header-wrapper">
-      <div className="navlink-wrapper">
-        <div className="navlink-container-left">
-          <Link
-            to="/"
-            className={"header-link " + (activeLink === "/" ? "current" : "")}
-          >
-            Home
-          </Link>
-          <Link
-            to="/#projects"
-            className={
-              "header-link " + (activeLink === "/#projects" ? "current" : "")
-            }
-          >
-            Projects
-          </Link>
-        </div>
-        <div className="navlink-container-right">
-          <Link
-            to="about"
-            className={
-              "header-link " + (activeLink === "/about" ? "current" : "")
-            }
-          >
-            About Me
-          </Link>
-          <Link
-            to="resume"
-            className={
-              "header-link " + (activeLink === "/resume" ? "current" : "")
-            }
-          >
-            Résumé
-          </Link>
-        </div>
-      </div>
       <div className="logo-container">
         <img
           src={image}
           alt="sword and trowel x-shaped cross logo"
           className="logo-image"
         />
+      </div>
+      <div className="navlink-wrapper">
+        <Link
+          to="/"
+          className={"header-link " + (activeLink === "/" ? "current" : "")}
+        >
+          Home
+        </Link>
+        <Link
+          to="/#projects"
+          className={
+            "header-link " + (activeLink === "/#projects" ? "current" : "")
+          }
+        >
+          Projects
+        </Link>
+        <Link
+          to="about"
+          className={
+            "header-link " + (activeLink === "/about" ? "current" : "")
+          }
+        >
+          About Me
+        </Link>
+        <Link
+          to="resume"
+          className={
+            "header-link " + (activeLink === "/resume" ? "current" : "")
+          }
+        >
+          Résumé
+        </Link>
       </div>
     </div>
   );
