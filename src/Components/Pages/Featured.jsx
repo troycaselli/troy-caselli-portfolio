4ef375d109740380f50a1e03f8820a432910017a
troycaselli <swordandtrowel1689@gmail.com>
2023-08-11 13:47:46 -0700

    added autoplay

--- src/Components/Pages/Featured.jsx
+++ src/Components/Pages/Featured.jsx
@@ -5,7 +5,7 @@ function Featured() {
     <section className="featured-landing-wrapper">
       <iframe
         className="featured-landing-video"
-        src="//www.youtube.com/embed/2eXqBfBBgFc?fs=0&rel=0&mute=1"
+        src="//www.youtube.com/embed/2eXqBfBBgFc?fs=0&rel=0&autoplay=1&mute=1"
         title="Asylum Contribution Video"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
