882123c0f0b842caec18dcaa8c8ca554cf5ac65e
troycaselli <swordandtrowel1689@gmail.com>
2023-08-17 14:55:08 -0700

    added srcset for responsive images

--- src/images/projectPreviews/index.js
+++ src/images/projectPreviews/index.js
@@ -1,8 +1,18 @@
-import asylumImg from "./asylum-tracker.png";
-import faithInFiveImg from "./faith-in-five.png";
-import famPromImg from "./family-promise-sms.png";
-import friendsImg from "./friends-app.png";
-import userImg from "./user-account-database.png";
+import asylumLg from "./asylum-tracker-1280.png";
+import asylumMd from "./asylum-tracker-800.png";
+import asylumSm from "./asylum-tracker-480.png";
+import faithInFiveLg from "./faith-in-five-1280.png";
+import faithInFiveMd from "./faith-in-five-800.png";
+import faithInFiveSm from "./faith-in-five-480.png";
+import famPromLg from "./family-promise-sms-1280.png";
+import famPromMd from "./family-promise-sms-800.png";
+import famPromSm from "./family-promise-sms-480.png";
+import friendsLg from "./friends-app-1280.png";
+import friendsMd from "./friends-app-800.png";
+import friendsSm from "./friends-app-480.png";
+import userLg from "./user-account-database-1280.png";
+import userMd from "./user-account-database-800.png";
+import userSm from "./user-account-database-480.png";
 import asylumImgBlur from "./asylum-tracker-blur.png";
 import faithInFiveImgBlur from "./faith-in-five-blur.png";
 import famPromImgBlur from "./family-promise-sms-blur.png";
@@ -10,11 +20,21 @@ import friendsImgBlur from "./friends-app-blur.png";
 import userImgBlur from "./user-account-database-blur.png";
 
 const images = {
-  asylumImg,
-  faithInFiveImg,
-  famPromImg,
-  friendsImg,
-  userImg,
+  asylumLg,
+  asylumMd,
+  asylumSm,
+  faithInFiveLg,
+  faithInFiveMd,
+  faithInFiveSm,
+  famPromLg,
+  famPromMd,
+  famPromSm,
+  friendsLg,
+  friendsMd,
+  friendsSm,
+  userLg,
+  userMd,
+  userSm,
   asylumImgBlur,
   faithInFiveImgBlur,
   famPromImgBlur,
