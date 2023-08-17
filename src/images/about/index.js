de59365db25b1f021713be03c89d126e3ee1152d
troycaselli <swordandtrowel1689@gmail.com>
2023-08-16 19:37:38 -0700

    decreased image sizes

--- src/images/about/index.js
+++ src/images/about/index.js
@@ -1,10 +1,10 @@
-import graduationImg from "./bloomtech-graduation.webp";
-import maternityImg from "./maternity.webp";
-import playingImg from "./play-and-study.webp";
-import glacierImg from "./glacier-national-park.webp";
-import cherriesImg from "./picking-cherries.webp";
-import studyingImg from "./juggling-responsibilities.webp";
-import mazeImg from "./corn-maze.webp";
+import graduationImg from "./bloomtech-graduation.jpg";
+import maternityImg from "./maternity.jpg";
+import playingImg from "./play-and-study.jpg";
+import glacierImg from "./glacier-national-park.jpg";
+import cherriesImg from "./picking-cherries.jpg";
+import studyingImg from "./juggling-responsibilities.jpg";
+import mazeImg from "./corn-maze.jpg";
 import graduationBlur from "./bloomtech-graduation-blur.jpg";
 import maternityBlur from "./maternity-blur.jpg";
 import playingBlur from "./play-and-study-blur.jpg";
@@ -12,7 +12,7 @@ import glacierBlur from "./glacier-national-park-blur.jpg";
 import cherriesBlur from "./picking-cherries-blur.jpg";
 import studyingBlur from "./juggling-responsibilities-blur.jpg";
 import mazeBlur from "./corn-maze-blur.jpg";
-import portraitImg from "./troy-caselli-portrait-square.jpg";
+import portraitImg from "./troy-caselli-portrait.jpg";
 import portraitBlur from "./troy-caselli-portrait-square-blur.jpg";
 
 const images = {
