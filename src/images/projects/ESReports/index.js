ddac321c8c5752180c6ed1e88e208b3b52f0a666
troycaselli <swordandtrowel1689@gmail.com>
2023-10-11 13:05:44 -0700

    added reports images

---
+++ src/images/projects/ESReports/index.js
@@ -0,0 +1,25 @@
+import allProvidersPage from "./all-providers-dash.png";
+import esProvidersPage from "./es-providers-dash.png";
+import fpsPreventionPage from "./fps-prevention-dash.png";
+import reportsMockUpsPage from "./reports-mock-ups.png";
+import splashPage from "./splash-page.png";
+import allProvidersPageBlur from "./all-providers-dash-blur.png";
+import esProvidersPageBlur from "./es-providers-dash-blur.png";
+import fpsPreventionPageBlur from "./fps-prevention-dash-blur.png";
+import reportsMockUpsPageBlur from "./reports-mock-ups-blur.png";
+import splashPageBlur from "./splash-page-blur.png";
+
+const images = {
+  allProvidersPage,
+  esProvidersPage,
+  fpsPreventionPage,
+  reportsMockUpsPage,
+  splashPage,
+  allProvidersPageBlur,
+  esProvidersPageBlur,
+  fpsPreventionPageBlur,
+  reportsMockUpsPageBlur,
+  splashPageBlur,
+};
+
+export default images;
