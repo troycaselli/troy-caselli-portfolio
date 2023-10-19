8ebf705a9d569127c4cac85fb69c534baea60b07
troycaselli <swordandtrowel1689@gmail.com>
2023-10-19 13:53:42 -0700

    initialized card;

--- src/Components/Pages/Featured.jsx
+++ src/Components/Pages/Featured.jsx
@@ -3,12 +3,14 @@ import "../../styling/featured.css";
 function Featured() {
   return (
     <section className="featured-landing-wrapper">
-      <iframe
+      <div className="featured-card"></div>
+      {/* car dummy, cathedral construction, blueprint */}
+      {/* <iframe
         className="featured-landing-video"
         src="//www.youtube.com/embed/2eXqBfBBgFc?fs=0&rel=0&autoplay=1&mute=1"
         title="Asylum Contribution Video"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
+      ></iframe> */}
     </section>
   );
 }
